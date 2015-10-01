bc7de5f84b15c582a231df79abde90c4db900e17
Waldinar Oliveira Neto <mzp_woliveira@uolinc.com>
2015-10-01 11:12:04 -0300

    instructions

---
+++ progfun-assignments/1.Instructions/example/.worksheet/src/teste.scala
@@ -0,0 +1,6 @@
+import example.Lists
+
+object teste {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(60); val res$0 = 
+  
+  Lists.max(List());System.out.println("""res0: Int = """ + $show(res$0))}
+}
